a535bc962df2597bdafe582da2c4cfce81935ff7
LSA31 <104567431+LSA31@users.noreply.github.com>
2026-01-06 10:28:45 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 280 ms, Memory: 9836 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> vect;
+vector<int> tmp;  // 임시 배열
+
+void mergeSort(int start, int end)
+{
+	if (end - start < 1)
+		return;
+
+	int M = (start + end) / 2;
+	mergeSort(start, M);
+	mergeSort(M + 1, end);
+
+	for (int i = start; i <= end; i++)
+		tmp[i] = vect[i];
+
+	int i = start;
+	int index1 = start, index2 = M + 1;
+	while (index1 <= M && index2 <= end)
+	{
+		if (tmp[index1] > tmp[index2])
+			vect[i++] = tmp[index2++];
+		else
+			vect[i++] = tmp[index1++];
+	}
+	while (index1 <= M)
+		vect[i++] = tmp[index1++];
+	while (index2 <= end)
+		vect[i++] = tmp[index2++];
+}
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	vect = vector<int>(n + 1, 0);
+	tmp = vector<int>(n + 1, 0);
+	for (int i = 1; i <= n; i++)
+		cin >> vect[i];
+
+	mergeSort(1, n);
+
+	for (int i = 1; i <= n; i++)
+		cout << vect[i] << '\n';
+}
