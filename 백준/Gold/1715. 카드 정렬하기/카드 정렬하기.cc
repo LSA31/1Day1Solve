47a8762b840f5835c5c567907208c12beeb5b7a0
LSA31 <104567431+LSA31@users.noreply.github.com>
2026-01-06 09:52:40 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 20 ms, Memory: 2920 KB -BaekjoonHub

---
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	priority_queue<int, vector<int>, greater<int>> pq; // 우선순위 큐
+	for (int i = 0; i < n; i++)
+	{
+		int data;
+		cin >> data;
+
+		pq.push(data);
+	}
+	int sum = 0;
+	while (pq.size() != 1)
+	{
+		int data1 = pq.top();
+		pq.pop();
+
+		int data2 = pq.top();
+		pq.pop();
+
+		sum += (data1 + data2);
+		pq.push(data1 + data2);
+	}
+	cout << sum;
+}
