2ad2a99e954a0686429902836d6f9946f933fb1d
LSA31 <104567431+LSA31@users.noreply.github.com>
2026-01-08 10:48:27 +0900

    [Silver II] Title: 유기농 배추, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int M, N, K;
+vector<vector<int>> graph;
+vector<vector<bool>> visited;
+
+int dy[4] = { -1, 0, 1, 0 };
+int dx[4] = { 0,-1, 0, 1 };
+
+
+/*
+if ((yy < 0 || N <= yy) || (xx < 0 || M <= xx)
+	|| visited[yy][xx] == true
+	|| graph[yy][xx] == 0)
+	continue;
+*/
+
+void dfs(int y, int x)
+{
+	graph[y][x] = 0;
+
+	for (int i = 0; i < 4; i++)
+	{
+		int yy = y + dy[i];
+		int xx = x + dx[i];
+
+		// 범위를 초과하거나 이동할 위치에 배추가 없다면
+		if ((yy < 0 || N <= yy) || (xx < 0 || M <= xx)
+			|| graph[yy][xx] == 0)
+			continue;
+
+		dfs(yy, xx);
+	}
+}
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int test_case = 0;
+	cin >> test_case;
+
+	for (int t = 0; t < test_case; t++)
+	{
+		cin >> M >> N >> K;
+
+		graph.resize(N, vector<int>(M, 0));
+		visited.resize(N, vector<bool>(M, false));
+
+		for (int i = 0; i < K; i++)
+		{
+			int y = 0, x = 0;
+			cin >> x >> y;
+
+			graph[y][x] = 1;
+		}
+
+		int cnt = 0;
+		for (int y = 0; y < N; y++)
+		{
+			for (int x = 0; x < M; x++)
+			{
+				if (graph[y][x] == 1)
+				{
+					dfs(y, x);
+					cnt++;
+				}
+			}
+		}
+		cout << cnt << '\n';
+
+		graph.clear();
+		visited.clear();
+		cnt = 0;
+	}
+
+	return 0;
+}
