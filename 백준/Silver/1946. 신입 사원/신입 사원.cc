93f7421a18e4f7af3c2de94eba847339f5114775
LSA31 <104567431+LSA31@users.noreply.github.com>
2026-02-01 22:24:24 +0900

    [Silver I] Title: 신입 사원, Time: 484 ms, Memory: 2920 KB -BaekjoonHub

---
+++ 백준/Silver/1946. 신입 사원/신입 사원.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>;
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int test_case = 0;
+	cin >> test_case;
+	for (int i = 0; i < test_case; i++)
+	{
+		int n = 0;
+		cin >> n;
+
+		vector<pair<int, int>> score(n);
+		for (int i = 0; i < n; i++)
+			cin >> score[i].first >> score[i].second;
+
+		sort(score.begin(), score.end());
+
+		int cnt = 1, Min = score[0].second;
+		for (int i = 1; i < n; i++)
+		{
+			if (score[i].second < Min)
+			{
+				Min = score[i].second;
+				cnt++;
+			}
+		}
+		cout << cnt << '\n';
+	}
+	return 0;
+}
