d93f6161af5970cee05838329ae74bf12f4d603e
LSA31 <104567431+LSA31@users.noreply.github.com>
2026-02-08 22:21:49 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.02 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int solution(int n)
+{
+    int ans = 1;
+    while(n > 1)
+    {
+        if(n % 2 != 0)
+        {
+            n--;
+            ans++;
+        }
+        else
+            n /= 2;
+    }
+
+    return ans;
+}
