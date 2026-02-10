9a9d15bb31ff7553c6693d44f43f2de3575c3c06
LSA31 <104567431+LSA31@users.noreply.github.com>
2026-02-10 23:58:17 +0900

    [Silver II] Title: 최대 힙, Time: 12 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n = 0;
+	cin >> n;
+
+	priority_queue<int> pq;
+	for (int i = 0; i < n; i++)
+	{
+		int x = 0;
+		cin >> x;
+
+		if (x == 0)
+		{
+			if (pq.empty())
+				cout << 0 << '\n';
+			else
+			{
+				cout << pq.top() << '\n';
+				pq.pop();
+			}
+		}
+		else
+			pq.push(x);
+	}
+
+	return 0;
+}
