3e59282c6daf03ea3e14e20ddfde52c683288eca
LSA31 <104567431+LSA31@users.noreply.github.com>
2026-02-17 21:53:54 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t = 0;
+	cin >> t;
+
+	for (int i = 0; i < t; i++)
+	{
+		int a = 0, b = 0;
+		cin >> a >> b;
+
+		cout << a + b << '\n';
+	}
+}
