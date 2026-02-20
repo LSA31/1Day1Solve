3a18d1f0af95337d702c8f191e9e62869aa2f9c0
LSA31 <104567431+LSA31@users.noreply.github.com>
2026-02-20 09:12:57 +0900

    [Silver III] Title: N과 M (4), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n, m;
+vector<int> picked;
+
+void backTracking(int start, int depth)
+{
+	if (depth == m)
+	{
+		for (int i = 0; i < picked.size(); i++)
+			cout << picked[i] << ' ';
+		cout << '\n';
+
+		return;
+	}
+	for (int i = start; i <= n; i++)
+	{
+		picked.push_back(i);
+		backTracking(i, depth + 1);
+		picked.pop_back();
+	}
+}
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n >> m;
+	backTracking(1, 0);
+
+	return 0;
+}
