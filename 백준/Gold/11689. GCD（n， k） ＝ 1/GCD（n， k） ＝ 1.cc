86bf41194f21da92954da59670efd84834da451b
LSA31 <104567431+LSA31@users.noreply.github.com>
2026-03-02 15:38:09 +0900

    [Gold I] Title: GCD(n, k) = 1, Time: 8 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.cc
+++ 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.cc
@@ -2,28 +2,30 @@
 #include <cmath>
 using namespace std;
 
+typedef long long LL;
+
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	long n;
+	LL n = 0;
 	cin >> n;
-
-	long ret = n;
-	for (long i = 2; i <= sqrt(n); i++)
+	
+	LL ret = n;
+	for (LL p = 2; p <= sqrt(n); p++)
 	{
-		if (n % i == 0) // i가 소인수인지 확인
+		if (n % p == 0)  // p가 소인수라면
 		{
-			ret = ret - ret / i; // 결괏값 업데이트
+			ret = ret - (ret / p);
 
-			while (n % i == 0) // 해당 소인수 지우기
-				n /= i;
+			while (n % p == 0)  // 해당 소인수 지우기
+				n /= p;
 		}
 	}
-	if (n > 1) // 아직 소인수 구성이 남아있는 경우
-		ret = ret - ret / n;
+	if (n > 1)
+		ret = ret - (ret / n);
 
 	cout << ret;
 }
