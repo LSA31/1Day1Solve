1708dd3f6b57421e4ab57f86188ad9e58f179b5c
LSA31 <104567431+LSA31@users.noreply.github.com>
2026-03-06 11:04:04 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<long long> vect;
+
+void fibonacci(int n)
+{
+	vect[1] = 1;
+
+	for (int i = 2; i <= n; i++)
+		vect[i] = (vect[i - 1] + vect[i - 2]);
+}
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n = 0;
+	cin >> n;
+	vect.resize(n + 1, 0);
+
+	fibonacci(n);
+
+	cout << vect[n];
+}
